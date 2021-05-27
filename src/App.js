2a3a03980392f5c3f97031e4ac2b505d816265bc
Enyabuto <elishahyperx@yahoo.com>
2021-05-27 15:01:11 -0500

    Updated Version

--- src/App.js
+++ src/App.js
@@ -1,98 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { Form, Card, Icon, Image } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import List from "./components/List";
+import withListLoading from "./components/withListLoading";
 function App() {
-  const [name, setName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [followers, setFollowers] = useState("");
-  const [following, setFollowing] = useState("");
-  const [repos, setRepos] = useState("");
-  const [avatar, setAvatar] = useState("");
-  const [userInput, setUserInput] = useState("");
-  const [error, setError] = useState("");
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
 
   useEffect(() => {
-    fetch("https://api.github.com/orgs/Netflix/repos")
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/orgs/Netflix/repos`;
+    fetch(apiUrl)
       .then((res) => res.json())
-      .then((data) => {
-        setData(data);
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
       });
-  }, []);
-
-  const setData = ({
-    name,
-    login,
-    followers,
-    following,
-    public_repos,
-    avatar_url,
-  }) => {
-    setName(name);
-    setUserName(login);
-    setFollowers(followers);
-    setFollowing(following);
-    setRepos(public_repos);
-    setAvatar(avatar_url);
-  };
-
-  const handleSearch = (e) => {
-    setUserInput(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    fetch(`https://api.github.com/orgs/Netflix/${userInput}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  };
-
+  }, [setAppState]);
   return (
-    <div>
-      <div className="navbar">Netflix Github API</div>
-      <div className="search">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Input
-              placeholder="Github user"
-              name="github user"
-              onChange={handleSearch}
-            />
-            <Form.Button content="Search" />
-          </Form.Group>
-        </Form>
+    <div className="App">
+      <div className="container">
+        <h1>My Repositories</h1>
       </div>
-      <div className="card">
-        <Card>
-          <Image src={avatar} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{name}</Card.Header>
-            <Card.Header>{userName}</Card.Header>
-          </Card.Content>
-
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {followers} Followers
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {repos} Repos
-            </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              {following} Following
-            </a>
-          </Card.Content>
-        </Card>
+      <div className="repo-container">
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
       </div>
+      <footer>
+        <div className="footer">
+          Built{" "}
+          <span role="img" aria-label="love">
+            💚
+          </span>{" "}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
